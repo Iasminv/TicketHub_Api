b35da96e085d7812ae3bccb8f320020fafe3f335
Iasminv <veroneziasmin@gmail.com>
2025-03-25 13:40:54 -0300

    Add project files.

---
+++ TicketHub_Api/Controllers/ConcertTicketsController.cs
@@ -0,0 +1,50 @@
+﻿using Azure.Storage.Queues;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Runtime.InteropServices.Marshalling;
+using System.Text.Json;
+
+namespace TicketHub_Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ConcertTicketsController : ControllerBase
+    {
+        private readonly ILogger<ConcertTicketsController> _logger;
+        private IConfiguration _configuration;
+
+        public ConcertTicketsController(ILogger<ConcertTicketsController> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Hello from ConcertTicketsController - GET");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(Ticket ticket)
+        {
+            // Validate ticket
+            if (string.IsNullOrEmpty(ticket.Name))
+            {
+                return BadRequest("Invalid Name");
+            }
+
+            string queueName = "tickets";
+            string connectionString = "connection string goes here";
+            // QueueClient queueClient = new QueueClient(connectionString, queueName);
+
+            // serialize an object to json
+            //string message = JsonSerializer.Serialize(someObject);
+
+            // send string message to queue
+            // await queueClient.SendMessageAsync("Hello from my api");
+
+            return Ok("Hello " + ticket.Name + ". Ticket sent to storage queue.");
+        }
+    }
+}
---
+++ TicketHub_Api/Ticket.cs
@@ -0,0 +1,47 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TicketHub_Api
+{
+    public class Ticket
+    {
+        public int ConcertId { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
+        public string Email { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Name is required")]
+        public string Name { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Phone Number is required")]
+        public string  Phone {  get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Should be at least one")]
+        public int Quantity {  get; set; }
+
+        [Required(ErrorMessage = "Credit Card Number is required")]
+        public string CreditCard {  get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Expiration is required")]
+        public string Expiration {  get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Security Code is required")]
+        public string SecurityCode { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Address is required")]
+        public string Address { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "City is required")]
+        public string City { get; set; } =string.Empty;
+
+        [Required(ErrorMessage = "Province is required")]
+        public string Province { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Postal Code is required")]
+        public string PostalCode { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Country is required")]
+        [MaxLength(60)]
+        public string Country { get; set; } = string.Empty;
+    }
+}
